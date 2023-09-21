fc407cf97d90d73a09c501298bcfd1989aa68ef6
Gael Hofman <Gieltie@outlook.fr>
2023-09-21 16:23:25 +0200

    encore erreur dans timeout, sortie pagination container du map slidecart

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,51 +7,54 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtB.date) < new Date(evtA.date) ? -1 : 1
+  const byDateDesc = data?.focus.sort(
+    (evtA, evtB) => (new Date(evtB.date) < new Date(evtA.date) ? -1 : 1) // inversee les evtA et evtB
   );
+
   const nextCard = () => {
     setTimeout(
       () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
       2500
-    );
+    ); // Ajout du -1 pour s'arreter a 2
   };
   useEffect(() => {
     nextCard();
   });
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((e, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+
+      {/* sortie le pagination container du map de slidecard */}
+
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event, radioIdx) => (
+            <input
+              key={event.date}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx} // change idx pour index
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
