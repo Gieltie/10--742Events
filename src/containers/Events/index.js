e91b6c9f4f7a24d98bf00b9dca51b175939a5a85
Gael Hofman <Gieltie@outlook.fr>
2023-09-28 08:51:54 +0200

    correction du filtre des evenements

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,7 +13,17 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = ((!type ? data?.events : data?.events) || []).filter(
+
+  const filteredEvents = (data?.events || [])
+    .filter((event) => {
+      if (!type || event.type === type) {
+        return true;
+      }
+      return false;
+    })
+    .slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE);
+
+  /* const filteredEvents = ((!type ? data?.events : data?.events) || []).filter(
     (event, index) => {
       if (
         (currentPage - 1) * PER_PAGE <= index &&
@@ -23,13 +33,15 @@ const EventList = () => {
       }
       return false;
     }
-  );
+  ); */
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
